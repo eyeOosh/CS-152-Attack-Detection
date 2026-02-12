fd9c7ba119f53d8e0fc34d7c7c928cfd2284f2f0
eyeOosh <109489470+eyeOosh@users.noreply.github.com>
2026-02-11 17:08:10 -0800

    simulated an attack
    attack points are red dots and non-attacked ones are blue dots

--- main.py
+++ main.py
@@ -32,14 +32,37 @@ print(categorical_df.shape)
 selected_cols = ["num_passengers", "purchase_lead", "length_of_stay", "flight_duration", "flight_hour"]
 subset_df = numeric_df[selected_cols]
 
-fig, axes = plt.subplots(1, len(subset_df.columns), figsize=(16, 5)) 
+#simulate attack
+attacked_df = subset_df.copy().astype(float)
+
+attack_fraction = 0.2 
+np.random.seed(42)
+attacked_mask = pd.DataFrame(False, index=subset_df.index, columns=subset_df.columns)
+
+for col in attacked_df.columns:
+    num_rows = len(attacked_df)
+    num_attack = int(num_rows * attack_fraction)
+    attack_indices = np.random.choice(num_rows, num_attack, replace=False)
+    attacked_mask.loc[attack_indices, col] = True
+    noise = np.random.normal(0, 5, size=num_attack)
+    attacked_df.loc[attack_indices, col] += noise
+    attacked_df[col] = np.maximum(attacked_df[col], 0)
+
+fig, axes = plt.subplots(1, len(subset_df.columns), figsize=(20,4))
 
 for i, col in enumerate(subset_df.columns):
-    counts = Counter(subset_df[col].dropna())
-    x = list(counts.keys())
-    y = list(counts.values())
-    
-    axes[i].scatter(x, y, color='skyblue', edgecolor='black')
+    x_values = attacked_df[col]
+    counts = Counter(x_values)
+    freq_x = list(counts.keys())
+    freq_y = list(counts.values())
+    freq_colors = []
+    for val in freq_x:
+        indices_with_val = attacked_df.index[x_values == val]
+        if attacked_mask.loc[indices_with_val, col].any():
+            freq_colors.append('red')
+        else:
+            freq_colors.append('blue')
+    axes[i].scatter(freq_x, freq_y, color=freq_colors, edgecolor='black')
     axes[i].set_title(col)
     axes[i].set_xlabel("Value")
     axes[i].set_ylabel("Frequency")
