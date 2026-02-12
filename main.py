6c557bb0d8efd409d13fd54c3a0adf06cfac53f5
eyeOosh <109489470+eyeOosh@users.noreply.github.com>
2026-02-11 17:36:26 -0800

    Added comments for Alan

--- main.py
+++ main.py
@@ -23,33 +23,50 @@ from sklearn.decomposition import PCA
 from collections import Counter
 from sklearn.metrics import classification_report, confusion_matrix
 
+#All comments are for Alan to understand what I did so far - 2/11/26
+
+#keep this seed consistent so we have preidctable models
+SEED = 42
+
+# data frame reading the csv file
 df = pd.read_csv("customer_booking.csv", encoding="ISO-8859-1")
 
+# seperate numerical and categorical data
 numeric_df = df.select_dtypes(include=["number"])
 categorical_df = df.select_dtypes(include=["object"])
 
+# for testing
 print(numeric_df.shape)
 print(categorical_df.shape)
 
+
+# we only want these numerical data columns for now
 selected_cols = ["num_passengers", "purchase_lead", "length_of_stay", "flight_duration", "flight_hour"]
 subset_df = numeric_df[selected_cols]
 
 #simulate attack
 attacked_df = subset_df.copy().astype(float)
 
+# attack 20% of the data
 attack_fraction = 0.2 
-np.random.seed(42)
+np.random.seed(SEED)
+#this stores a table of if a data point is attacked or not, initally all are not
 attacked_mask = pd.DataFrame(False, index=subset_df.index, columns=subset_df.columns)
 
 for col in attacked_df.columns:
     num_rows = len(attacked_df)
     num_attack = int(num_rows * attack_fraction)
+    # choose a random index to attack
     attack_indices = np.random.choice(num_rows, num_attack, replace=False)
+    #make that index True in the attack_mask table
     attacked_mask.loc[attack_indices, col] = True
+    #create attack here
     noise = np.random.normal(0, 5, size=num_attack)
     attacked_df.loc[attack_indices, col] += noise
+    #attack is non-negative
     attacked_df[col] = np.maximum(attacked_df[col], 0)
 
+#all plotting is here
 fig, axes = plt.subplots(1, len(subset_df.columns), figsize=(20,4))
 
 for i, col in enumerate(subset_df.columns):
@@ -77,12 +94,18 @@ plt.show()
 X = attacked_df.values.flatten().reshape(-1, 1)
 y = attacked_mask.values.flatten().astype(int)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=SEED)
 
-clf = RandomForestClassifier(n_estimators=100, random_state=42)
+clf = RandomForestClassifier(n_estimators=100, random_state=SEED)
 clf.fit(X_train, y_train)
 
 y_pred = clf.predict(X_test)
 
 print("Classification Report:\n", classification_report(y_test, y_pred))
+
+# This returns in the format
+# [[TN, FN
+#   FP, TP]]
+# where TN = true negative, FN = false negative
+#       FP = false positive, TP = true positive
 print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
