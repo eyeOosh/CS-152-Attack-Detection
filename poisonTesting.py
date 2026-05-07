88dd43a95b3ecf30327958caa0557f37f7363c18
eyeOosh <109489470+eyeOosh@users.noreply.github.com>
2026-05-07 16:13:31 -0700

    Update poisonTesting.py

--- poisonTesting.py
+++ poisonTesting.py
@@ -7,7 +7,6 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, TensorDataset
-import detectAttackType as dat
 
 show_plots = False
 
